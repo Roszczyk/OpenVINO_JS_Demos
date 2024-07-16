8b7842b2e43d96325c91dc91725988bd80db7227
roszczyk <mikolaj.roszczyk@intel.com>
2024-07-16 16:35:55 +0200

    2 webcams - one to be transformed into image after inference

--- 001-app/src/renderer.js
+++ 001-app/src/renderer.js
@@ -2,31 +2,50 @@ document.addEventListener('DOMContentLoaded', () => {
   const toggleWebcamButton = document.getElementById('toggleWebcamButton');
   const webcamElement = document.getElementById('webcam');
   let webcamStream = null;
+  const toggleWebcamButton2 = document.getElementById('toggleWebcamButton2');
+  const webcamElement2 = document.getElementById('webcam2');
+  let webcamStream2 = null;
 
   toggleWebcamButton.addEventListener('click', () => {
     if (webcamStream) {
-      stopWebcam();
+      stopWebcam(webcamElement, webcamStream, toggleWebcamButton, "1");
+      webcamStream = null;
     } else {
-      startWebcam();
+      startWebcam(webcamElement, stream => {
+        webcamStream = stream;
+        toggleWebcamButton.textContent = 'Stop Webcam 1';
+      });
     }
   });
 
-  function startWebcam() {
+  toggleWebcamButton2.addEventListener('click', () => {
+    if (webcamStream2) {
+      stopWebcam(webcamElement2, webcamStream2, toggleWebcamButton2, "2");
+      webcamStream2 = null;
+    } else {
+      startWebcam(webcamElement2, stream => {
+        webcamStream2 = stream;
+        toggleWebcamButton2.textContent = 'Stop Webcam 2';
+      });
+    }
+  });
+
+  function startWebcam(videoElement, onStreamReady) {
     navigator.mediaDevices.getUserMedia({ video: true })
       .then(stream => {
-        webcamElement.srcObject = stream;
-        webcamStream = stream;
-        toggleWebcamButton.textContent = 'Stop Webcam';
+        videoElement.srcObject = stream;
+        onStreamReady(stream);
       })
       .catch(error => {
         console.error('Error accessing webcam:', error);
       });
   }
 
-  function stopWebcam() {
-    webcamStream.getTracks().forEach(track => track.stop());
-    webcamElement.srcObject = null;
-    webcamStream = null;
-    toggleWebcamButton.textContent = 'Start Webcam';
+  function stopWebcam(videoElement, stream, buttonElement, number) {
+    stream.getTracks().forEach(track => track.stop());
+    videoElement.srcObject = null;
+    buttonElement.textContent = 'Start Webcam ' + number;
   }
 });
+
+
