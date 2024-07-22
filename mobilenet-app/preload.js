42108b44af082f3fff53164e51a800ba43aaff30
roszczyk <mikolaj.roszczyk@intel.com>
2024-07-22 14:37:05 +0200

    Work on code clearance and attemts to run openVINO inside

--- mobilenet-app/preload.js
+++ mobilenet-app/preload.js
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   openFileDialog: () => ipcRenderer.invoke('open-file-dialog'),
-  runInference: (imagePath, device) => ipcRenderer.invoke('run-inference', imagePath, device)
+  runInference: (imagePath, device) => ipcRenderer.invoke('run-inference', imagePath, device),
+  ov : ov
 });
